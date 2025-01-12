2332d8779adf82bd62efbad6bf2c2364cde0199c
David Zywiec <dzlast95@gmail.com>
2025-01-12 10:18:23 -0600

    Started on a State

--- Scenes/Animal/animal.gd
+++ Scenes/Animal/animal.gd
@@ -1,7 +1,11 @@
 extends RigidBody2D
 
+enum ANIMAL_STATE {READY, DRAG, RELEASE}
+var _state : ANIMAL_STATE = ANIMAL_STATE.READY
+
 @onready
 var onscreen_notifier : VisibleOnScreenNotifier2D = $OffScreenNotifier
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +13,43 @@ func _ready() -> void:
 
 
 func _physics_process(delta):
-	pass
+	update(delta)
+
+
+func on_input_event(viewport, event, shape_idx):
+	print(event)
+
+
+func update(delta) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG:
+			update_drag()
+				
+
+func update_drag() -> void:
+	if detect_release() == true:
+		return
+		
+	var gmp = get_global_mouse_position()
+	position = gmp
+
+
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released("drag"):
+			set_new_state(ANIMAL_STATE.RELEASE)
+			return true
+			
+	return false
+
+
+func set_new_state(new_state: ANIMAL_STATE) -> void:
+	_state = new_state
+	if _state == ANIMAL_STATE.RELEASE:
+		freeze = false
+	elif _state == ANIMAL_STATE.DRAG:
+		pass
+
 		
 func die() -> void:
 	SignalManager.die.emit()
